9c313d41dbe52d77ed62162a3df5503bedb0390d
Toma Žulj <tomazulj10@gmail.com>
2024-12-13 17:09:38 +0100

    3.6. Sending Error Messages

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,9 @@ func (app *application) routes() http.Handler {
 	// Initialize a new httprouter router instance.
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	// Register the relevant methods, URL patterns and handler functions for our
 	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
 	// http.MethodPost are constants which equate to the strings "GET" and "POST"
@@ -18,6 +21,6 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
-	// Return the httprouter instance.
-	return router
+	// Wrap the router with the panic recovery middleware.
+	return app.recoverPanic(router)
 }
